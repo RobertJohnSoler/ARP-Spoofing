0a925da030aa8b597da8ec4ab17d1c20668d9d0a
RobertJohnSoler <rsoler@hawk.iit.edu>
2024-11-29 10:00:00 -0600

    added a few comments

--- scratch_files/inject.c
+++ scratch_files/inject.c
@@ -9,7 +9,7 @@ int main(){
     char errbuf[PCAP_ERRBUF_SIZE];      // just a buffer to hold the error message, if any
     pcap_t *handler = pcap_open_live(interface, BUFSIZ, 1, 1000, errbuf); // enable the chosen interface to handle the capturing and sending of packets
 
-    if (handler == NULL){
+    if (handler == NULL){               // check if the device was enabled successfully
         printf("Error opening device!");
         return 1;
     } else {
