f9f3ef375c2e828a43f1354980163f4b8713239c
RobertJohnSoler <rsoler@hawk.iit.edu>
2025-01-05 22:41:54 -0600

    added comments to linux ip router

--- linux_ip_router.py
+++ linux_ip_router.py
@@ -1,10 +1,11 @@
-# This file 
+# This class is responsible for enable ip routing in the attacker's computer (if it's running on Linux)
 
 class Linux_IP_Router():
 
     def __init__(self):
         self.file_path = "/proc/sys/net/ipv4/ip_forward"
 
+    # Function to tell your machine to enable IP routing
     def start(self):
         """
         Enables IP route ( IP Forward ) in linux-based distro
@@ -17,7 +18,7 @@ class Linux_IP_Router():
         with open(self.file_path, "w") as f:
             print(1, file=f)
 
-
+    # Function to tell your machine to stop IP routing
     def stop(self):
         """
         Enables IP route ( IP Forward ) in linux-based distro
